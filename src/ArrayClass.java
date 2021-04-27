c6251ba5b1db0fc6f19a0ed4331eef5d7bd425c1
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-04-27 21:57:25 +0530

    Array traversal using traverse function

--- src/ArrayClass.java
+++ src/ArrayClass.java
@@ -1,5 +1,13 @@
 public class ArrayClass {
+    int[] ar = new int[5];                      //declare array
+    int[] arr = {10,20,30,40,50,60};            //declaring array using alternate method
+    public void traverse(){
+        for(int i =0; i<arr.length;i++){
+            System.out.println("Element at index "+i+" is "+arr[i]);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello and welcome to the journey of DSA");
+        ArrayClass arrobj = new ArrayClass();
+        arrobj.traverse();
     }
 }
