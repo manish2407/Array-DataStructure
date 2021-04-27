306431e483d95353f2fe7d196f3bea549328c5be
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-04-27 12:23:33 +0530

    basic structure of code

--- src/ArrayClass.java
+++ src/ArrayClass.java
@@ -1,5 +1,5 @@
 public class ArrayClass {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Hello and welcome to the journey of DSA");
     }
 }
