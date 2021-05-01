7d94fae7cc751fa3cb2374fb1d25b288d2d50bdf
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-05-01 20:00:22 +0530

    Constructor of ArrayInterface

--- src/ArrayUsingInterface/ArrayInterface.java
+++ src/ArrayUsingInterface/ArrayInterface.java
@@ -1,4 +1,11 @@
 package ArrayUsingInterface;
 
 public class ArrayInterface {
+        private long[] a;                   //referance to array a
+        private int ElementCount;           //variable for ElementCount
+        public ArrayInterface(int size){    //Constructor
+            a = new long[size];
+            ElementCount = 0;
+    }
+
 }
--- src/ArrayUsingInterface/ArrayMain.java
+++ src/ArrayUsingInterface/ArrayMain.java
@@ -1,6 +1,2 @@
 package ArrayUsingInterface;
 
-public class ArrayMain {
-    private long[] a;                   //referance to array
-    private int ElementCount;           //variable for ElementCount
-}
