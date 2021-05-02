99cc1e2a17919e92a5646a3f92cf395b79bc2fae
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-05-02 10:03:23 +0530

    insert function

--- src/ArrayUsingInterface/ArrayInterface.java
+++ src/ArrayUsingInterface/ArrayInterface.java
@@ -43,6 +43,9 @@ public class ArrayInterface {
                 ElementCount--;
             }
     }
-
+    public void insert(long value){
+        a[ElementCount]= value;
+        ElementCount++;
+    }
 }
 
