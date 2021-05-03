a17219cd24d7a837cd020dc6ae8fb9f65ed6bc51
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-05-03 21:58:36 +0530

    Constructor

--- src/ObjectClassArray.java
+++ src/ObjectClassArray.java
@@ -1,2 +1,8 @@
 public class ObjectClassArray {
+    private ObjectClass[] arr;                  //reference to array
+    private int Nelements;
+    public ObjectClassArray(int size){
+        arr = new ObjectClass[size];
+        Nelements =0;
+    }
 }
