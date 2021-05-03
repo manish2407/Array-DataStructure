c48ca148a3b57e544d4880accb4ef6ffee371674
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-05-03 22:20:42 +0530

    Search function

--- src/ObjectClassArray.java
+++ src/ObjectClassArray.java
@@ -9,6 +9,17 @@ public class ObjectClassArray {
         arr[Nelements] = new ObjectClass(First, Last,age1);
         Nelements++;
     }
+    public  void search(String searchname){
+        int j;
+        for (j = 0; j < Nelements ; j++) {
+            arr[j].getLastName().equals("Tiwari");
+            System.out.println("Object found at "+j+" position");
+        }
+        if(j==Nelements){
+            System.out.println("Object is not present in array");
+        }
+
+    }
     public void display(){
         for (int i = 0; i < Nelements ; i++) {
             arr[i].display();
